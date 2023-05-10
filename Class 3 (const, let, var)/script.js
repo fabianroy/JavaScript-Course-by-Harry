353fb9a724075fae208a6bc094deeff8de923054
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2023-05-11 01:03:37 +0600

    Create script.js

---
+++ Class 3 (const, let, var)/script.js
@@ -0,0 +1,18 @@
+var a = 1;
+var a = 2;
+a = 3;
+
+let b = "2";
+
+const author = 'Fabian Roy';
+
+let c = null;
+let d = "hello";
+let e = undefined;
+
+{
+    let d = "hola";
+    console.log(d)
+}
+
+console.log(d);
