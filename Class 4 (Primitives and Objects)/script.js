d6cb64e855d6917d90bd7b15208099e8181feeb1
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2023-05-11 01:04:53 +0600

    Create script.js

---
+++ Class 4 (Primitives and Objects)/script.js
@@ -0,0 +1,12 @@
+//nn bb ss u 
+
+let a = null;
+let b = 354;
+let c = true;
+let d = BigInt("567") + BigInt("3");
+let e = "fabro";
+let f = Symbol("I am a nice symbol");
+let g = undefined;
+
+console.log(a, b, c, d, e ,f ,g);
+console.log(typeof a, typeof b, typeof c, typeof d, typeof e, typeof f, typeof g);
