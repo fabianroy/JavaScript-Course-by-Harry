b29f75fc3897bd28cf943ba77b1772409458e0f4
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2023-05-11 01:08:26 +0600

    Create script.js

---
+++ Class 6 (Operator)/script.js
@@ -0,0 +1,32 @@
+//Arithmatic Operators
+
+let a = 10;
+let b = 5;
+console.log(a+b);
+console.log(a++);
+
+
+//Assignment Operators
+
+let assignment = 10;
+assignment += 5;
+console.log(assignment);
+
+
+//Comparison Operatiors
+
+let x = 10;
+let y = "10";
+
+console.log(x==y);
+console.log(x!=y);
+console.log(x!==y);
+console.log(x===y);
+
+
+//Logical Operators
+
+let f = 5;
+let n = 5;
+console.log(f=n && f==5);
+console.log(f=n || f==5); 
