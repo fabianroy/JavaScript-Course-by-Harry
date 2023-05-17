1bb9887b7d0b09868bb9cca174681ff074e0186a
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2023-05-17 22:09:36 +0600

    Update script.js

--- Class 7 (Conditional expressions)/script.js
+++ Class 7 (Conditional expressions)/script.js
@@ -1,1 +1,11 @@
-
+let a = prompt("Hey what's your age? ");
+a = Number.parseInt(a); //converting a string to a number
+if(a>=18 && a<=100){
+  alert("You're eligible to vote.");
+}
+else if(a>100){
+  alert("I think you're DEAD!! Rest is peace.");
+}
+else{
+  alert("You're ineligible to vote.");
+}
