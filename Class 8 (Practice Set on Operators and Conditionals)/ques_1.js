411b0b2e6b06ede7b5296c672559767b13b450a8
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2023-05-18 18:11:31 +0600

    Update ques_1.js

--- Class 8 (Practice Set on Operators and Conditionals)/ques_1.js
+++ Class 8 (Practice Set on Operators and Conditionals)/ques_1.js
@@ -2,7 +2,7 @@
 // Use logical operators to find whether the age of a person lies between 10 ad 20. 
 
 let age = prompt("What is your age?");
-age = Number.parseTnt(age);
+age = Number.parseInt(age);
 if(age>10 && age<20){
   console.log("Your age lies between 10 and 20")
 }
