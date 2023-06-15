6386c6185cf262a2b2897b677ea8c7d657955dbd
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2023-06-15 13:40:26 +0600

    Delete ques_5.js

--- Class 8 (Practice Set on Operators and Conditionals)/ques_5.js
+++
@@ -1,6 +0,0 @@
-// Question 5 **
-// Print "You can drive" or "You can't" based on age being greater or equal than 18 using rernary operator.
-
-let age = prompt("What is your age?");
-let a = age>=18? "You are eligible to get your driving license": "You are not eligible to get your driving license."
-console.log(a);
