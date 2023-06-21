65650be18dc123bf5e00c1a5ce594cd694e834e1
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2023-06-21 20:17:09 +0600

    Update script.js

--- Class 11 (Functions)/script.js
+++ Class 11 (Functions)/script.js
@@ -1,1 +1,18 @@
+function calc(x, y){
+  return (x+y)/2;
+}
 
+//Arrow Function
+const hello = ()=>{
+  console.log("Welcome to the function of JavaScript");
+  return;
+}
+
+let a = 5;
+let b = 10;
+let c = 15;
+
+hello();
+console.log("Average of a & b = " + calc(a, b));
+console.log("Average of a & b = " + calc(b,c));
+console.log("Average of a & b = " + calc(a, c));
