84cae96a31f9217cc88d88b98418b31878691639
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2023-06-21 20:52:07 +0600

    Create ques_2.js

---
+++ Class 12 (Practice Set)/ques_2.js
@@ -0,0 +1,12 @@
+// Question #2
+// Write a program to print the marls of 4 stuents in an object using for in loop.
+
+let marks = {
+  Nasif: 90,
+  Fabian: 88,
+  Mujtabir: 70,
+  Mahir: 60
+}
+for(let i in marks){
+  console.log("The marks of " + i + " is " + marks[i]);
+}
