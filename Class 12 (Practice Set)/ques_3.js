b46d8f73b414550ea389f1f67336e5396ce5517f
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2023-06-21 21:11:14 +0600

    Create ques_3.js

---
+++ Class 12 (Practice Set)/ques_3.js
@@ -0,0 +1,10 @@
+// Question #3
+// Write a program to print "Try Again" until the user enters correct number.
+
+let number = 10;
+let i
+while(i != number){
+  console.log("Try Again");
+  i = prompt("Enter the correct number");
+}
+console.log("The number you've entered is correct !")
