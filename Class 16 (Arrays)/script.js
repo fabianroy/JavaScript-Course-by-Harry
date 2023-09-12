e9a2334bc3e785638226638ccfd0b8aee791d6e3
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2023-09-12 20:13:09 +0600

    Create script.js

---
+++ Class 16 (Arrays)/script.js
@@ -0,0 +1,8 @@
+let class_12 = [76, 83, 86, 78, "Not Present"]
+console.log(class_12[3]);
+class_12[2] = 85; // Changing the element of the array
+console.log(class_12[5]); // Will be undefined
+class_12[5] = 91; // Adding new element
+console.log(class_12);
+console.log("The length is", class_12.length);
+console.log(typeof class_12);
