814e1ed82ede1ed44c26ae29582f7938fac31f0c
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2023-10-01 19:35:44 +0600

    Create script.js

---
+++ Class 25 (Alert, Promt & Confirm)/script.js
@@ -0,0 +1,10 @@
+alert("Welcome to JS Practice"); 
+
+let a = prompt("Enter your age");
+a = Number.parseInt(a);
+let write = confirm("Do you want to write your age on the document?")
+if(write){
+  document.write("Your age is : " + a);
+}else{
+  document.write("Please allow me to write")
+}
